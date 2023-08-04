1a7f4219c0a80d1d692b0d1252833af7e58e5e56
boonewr <boonewr@guilford.edu>
2023-08-04 14:25:55 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,7 +11,11 @@ urlpatterns = [
 
     # path for about view
 
+    path('about/', views.about, name='about'),
+
     # path for contact us view
+
+    path('contact/', views.contact, name='contact'),
 
     # path for registration
 
