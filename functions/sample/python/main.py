3d6ad7e361440c133c10cf19e1d7362be9085ecd
boonewr <boonewr@guilford.edu>
2023-08-09 16:21:28 -0400

    Functions again

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -6,6 +6,15 @@ Returns:
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 import requests
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+couch_username = os.getenv("COUCH_USERNAME")
+iam_api_key = os.getenv("IAM_API_KEY")
+
+print("Loading function")
 
 
 def main(param_dict):
@@ -19,19 +28,21 @@ def main(param_dict):
         _type_: _description_ TODO
     """
 
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
+    # try:
+    #     client = Cloudant.iam(
+    #         account_name=couch_username,
+    #         api_key=iam_api_key,
+    #         connect=True,
+    #     )
+    #     print(f"Databases: {client.all_dbs()}")
+    # except CloudantException as cloudant_exception:
+    #     print("unable to connect")
+    #     return {"error": cloudant_exception}
+    # except (requests.exceptions.RequestException, ConnectionResetError) as err:
+    #     print("connection error")
+    #     return {"error": err}
 
-    print({"dbs": client.all_dbs()})
-    return {"dbs": client.all_dbs()}
+    # print({"dbs": client.all_dbs()})
+    # return {"dbs": client.all_dbs()}
+
+main()
